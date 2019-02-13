d1fcd23a6aa37634c1b0d4eecb3b49629ae92084
arenaslozoya <aalarenas1800@gmail.com>
2019-02-13 09:09:36 -0600

    Initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, public http: HttpClient) {
+    this.http.get('v1/')
+
+  }
+
+}
